a73075088e4f48ca65f1a37fa7cd8bdcb90f8f57
dmcdaniel90 <dmcdaniel9@gmail.com>
2024-01-16 18:21:43 +0000

    Updated Login style

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,8 +7,12 @@ const colors = {
   styles: {
     global: {
       body: {
-        bg: '#d3d3d3'
+        bgGradient: 'linear(135deg, #2274a5 21px, #e7dfc6 22px, #e7dfc6 24px, transparent 24px, transparent 67px, #e7dfc6 67px, #e7dfc6 69px, transparent 69px), linear-gradient(225deg, #2274a5 21px, #e7dfc6 22px, #e7dfc6 24px, transparent 24px, transparent 67px, #e7dfc6 67px, #e7dfc6 69px, transparent 69px)0 64px',
+        bg: '#2274a5',
+        bgSize: '64px 128px'
+        
       }
+      
     }
   },
   colors: {
